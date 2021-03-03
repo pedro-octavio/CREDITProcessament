fca3efe41ef65a0545a9dd355f6885df61dbb6c4
pedro-octavio <pedrooctavio.dev@outlook.com>
2021-03-03 15:20:28 -0300

    incorrect name changed in configuration ioc

--- src/CREDITProcessament.Infra.IOC/ConfigurationIOC.fs
+++ src/CREDITProcessament.Infra.IOC/ConfigurationIOC.fs
@@ -9,16 +9,17 @@ open CREDITProcessament.Domain.Mappers
 open CREDITProcessament.Domain.Services.Services
 open CREDITProcessament.Domain.Validators
 open FluentValidation
+open CREDITProcessament.Domain.Models.RequestModels
 
 type ConfigurationIOC() = class
 
     static member Load(containerBuilder : ContainerBuilder) =
         containerBuilder.RegisterAutoMapper(typeof<MappingProfile>.Assembly) |> ignore
 
-        containerBuilder.RegisterType<AddUserValidator>().As<IValidator<AddUserValidator>>() |> ignore
-        containerBuilder.RegisterType<DeleteUserValidator>().As<IValidator<DeleteUserValidator>>() |> ignore
-        containerBuilder.RegisterType<GetUserByCPFValidator>().As<IValidator<GetUserByCPFValidator>>() |> ignore
-        containerBuilder.RegisterType<UpdateUserValidator>().As<IValidator<UpdateUserValidator>>() |> ignore
+        containerBuilder.RegisterType<AddUserValidator>().As<IValidator<AddUserRequestModel>>() |> ignore
+        containerBuilder.RegisterType<DeleteUserValidator>().As<IValidator<DeleteUserRequestModel>>() |> ignore
+        containerBuilder.RegisterType<GetUserByCPFValidator>().As<IValidator<GetUserByCPFRequestModel>>() |> ignore
+        containerBuilder.RegisterType<UpdateUserValidator>().As<IValidator<UpdateUserRequestModel>>() |> ignore
 
         containerBuilder.RegisterType<UserRepository>().As<IUserRepository>() |> ignore
         containerBuilder.RegisterType<UserService>().As<IUserService>() |> ignore
