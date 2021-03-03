2435e50a65e50d3af0971a2bab01f7a6b97b17b4
pedro-octavio <pedrooctavio.dev@outlook.com>
2021-03-03 10:21:12 -0300

    created response models of user

---
+++ src/CREDITProcessament.Domain/Models/ResponseModels/GetAllUsersResponseModel.cs
@@ -0,0 +1,14 @@
+﻿namespace CREDITProcessament.Domain.Models.ResponseModels
+{
+    public class GetAllUsersResponseModel
+    {
+        public GetAllUsersResponseModel(string cpf, string name)
+        {
+            CPF = cpf;
+            Name = name;
+        }
+
+        public string CPF { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ src/CREDITProcessament.Domain/Models/ResponseModels/GetUserByCPFResponseModel.cs
@@ -0,0 +1,12 @@
+﻿namespace CREDITProcessament.Domain.Models.ResponseModels
+{
+    public class GetUserByCPFResponseModel
+    {
+        public GetUserByCPFResponseModel(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+}
