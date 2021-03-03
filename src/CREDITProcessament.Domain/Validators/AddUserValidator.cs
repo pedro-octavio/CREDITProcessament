d9eba32c8604bc989bcd54b68d83b12e4ba1744a
pedro-octavio <pedrooctavio.dev@outlook.com>
2021-03-03 14:16:55 -0300

    validators for user request models, created

---
+++ src/CREDITProcessament.Domain/Validators/AddUserValidator.cs
@@ -0,0 +1,21 @@
+﻿using CREDITProcessament.Domain.Models.RequestModels;
+using FluentValidation;
+
+namespace CREDITProcessament.Domain.Validators
+{
+    public class AddUserValidator : AbstractValidator<AddUserRequestModel>
+    {
+        public AddUserValidator()
+        {
+            RuleFor(x => x.CPF)
+                .NotNull().WithMessage("The CPF cannot be null.")
+                .NotEmpty().WithMessage("The CPF cannot be empty.")
+                .Length(11).WithMessage("The of lenghtn of CPF is 11 caracters");
+
+            RuleFor(x => x.Name)
+                .NotNull().WithMessage("The Name cannot be null.")
+                .NotEmpty().WithMessage("The Name cannot be empty.")
+                .MaximumLength(120).WithMessage("The maximum lenght of Name is 120 caracters");
+        }
+    }
+}
