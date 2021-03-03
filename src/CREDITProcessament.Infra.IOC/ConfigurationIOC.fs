7a5f43b62129def48675018ec05edda56cdaf518
pedro-octavio <pedrooctavio.dev@outlook.com>
2021-03-03 12:09:52 -0300

    base of ioc configuration created

---
+++ src/CREDITProcessament.Infra.IOC/ConfigurationIOC.fs
@@ -0,0 +1,13 @@
+﻿namespace CREDITProcessament.Infra.IOC
+
+open Autofac
+
+open CREDITProcessament.Data.Repository.Repositories
+open CREDITProcessament.Data.Core.Interfaces
+
+type ConfigurationIOC() = class
+
+    static member Load(containerBuilder : ContainerBuilder) =
+        containerBuilder.RegisterType<UserRepository>().As<IUserRepository>()
+
+end
