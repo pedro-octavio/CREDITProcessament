b3d6bf4ef7b24112db5923b8ce8e3393ccc85fc8
pedro-octavio <pedrooctavio.dev@outlook.com>
2021-03-03 11:08:29 -0300

    mappers for user created

---
+++ src/CREDITProcessament.Domain/Mappers/MappingProfile.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using CREDITProcessament.Data.Models;
+using CREDITProcessament.Domain.Models.RequestModels;
+using CREDITProcessament.Domain.Models.ResponseModels;
+
+namespace CREDITProcessament.Domain.Mappers
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            #region RequestModels
+            CreateMap<AddUserRequestModel, UserModel>()
+                .ForMember(dest => dest.CPF, opts => opts.MapFrom(src => src.CPF))
+                .ForMember(dest => dest.Name, opts => opts.MapFrom(src => src.Name));
+
+            CreateMap<UpdateUserRequestModel, UserModel>()
+                .ForMember(dest => dest.CPF, opts => opts.MapFrom(src => src.CPF))
+                .ForMember(dest => dest.Name, opts => opts.MapFrom(src => src.Name));
+            #endregion
+
+            #region ResponseModels
+            CreateMap<UserModel, GetAllUsersResponseModel>()
+                .ForMember(dest => dest.CPF, opts => opts.MapFrom(src => src.CPF))
+                .ForMember(dest => dest.Name, opts => opts.MapFrom(src => src.Name));
+
+            CreateMap<UserModel, GetUserByCPFResponseModel>()
+                .ForMember(dest => dest.Name, opts => opts.MapFrom(src => src.Name));
+            #endregion
+        }
+    }
+}
