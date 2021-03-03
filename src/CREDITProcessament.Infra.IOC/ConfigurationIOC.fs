b17a1fac37aa3dc49f7c773a17a830e5c2756ae3
pedro-octavio <pedrooctavio.dev@outlook.com>
2021-03-03 14:19:50 -0300

    ioc configuration implemented

--- src/CREDITProcessament.Infra.IOC/ConfigurationIOC.fs
+++ src/CREDITProcessament.Infra.IOC/ConfigurationIOC.fs
@@ -1,13 +1,26 @@
 ﻿namespace CREDITProcessament.Infra.IOC
 
+open AutoMapper.Contrib.Autofac.DependencyInjection
 open Autofac
-
-open CREDITProcessament.Data.Repository.Repositories
 open CREDITProcessament.Data.Core.Interfaces
+open CREDITProcessament.Data.Repository.Repositories
+open CREDITProcessament.Domain.Core.Interfaces
+open CREDITProcessament.Domain.Mappers
+open CREDITProcessament.Domain.Services.Services
+open CREDITProcessament.Domain.Validators
+open FluentValidation
 
 type ConfigurationIOC() = class
 
     static member Load(containerBuilder : ContainerBuilder) =
-        containerBuilder.RegisterType<UserRepository>().As<IUserRepository>()
+        containerBuilder.RegisterAutoMapper(typeof<MappingProfile>.Assembly) |> ignore
+
+        containerBuilder.RegisterType<AddUserValidator>().As<IValidator<AddUserValidator>>() |> ignore
+        containerBuilder.RegisterType<DeleteUserValidator>().As<IValidator<DeleteUserValidator>>() |> ignore
+        containerBuilder.RegisterType<GetUserByCPFValidator>().As<IValidator<GetUserByCPFValidator>>() |> ignore
+        containerBuilder.RegisterType<UpdateUserValidator>().As<IValidator<UpdateUserValidator>>() |> ignore
+
+        containerBuilder.RegisterType<UserRepository>().As<IUserRepository>() |> ignore
+        containerBuilder.RegisterType<UserService>().As<IUserService>() |> ignore
 
 end
