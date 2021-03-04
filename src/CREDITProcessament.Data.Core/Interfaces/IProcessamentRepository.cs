5055d8c491c9d4a3d330b3b1dd40e59f55de067d
pedro-octavio <pedrooctavio.dev@outlook.com>
2021-03-04 09:57:29 -0300

    processament repository interface created

---
+++ src/CREDITProcessament.Data.Core/Interfaces/IProcessamentRepository.cs
@@ -0,0 +1,17 @@
+﻿using CREDITProcessament.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CREDITProcessament.Data.Core.Interfaces
+{
+    public interface IProcessamentRepository
+    {
+        Task<IEnumerable<ProcessamentModel>> GetAllAsync(DateTime? startDate, DateTime? endDate);
+        Task<IEnumerable<ProcessamentModel>> GetAllAsync(bool processed);
+        Task AddAsync(ProcessamentModel processament);
+        Task UpdateAsync(ProcessamentModel processament);
+        Task DeleteAsync(ProcessamentModel processament);
+        Task DeleteRangeAsync(IEnumerable<ProcessamentModel> processaments);
+    }
+}
