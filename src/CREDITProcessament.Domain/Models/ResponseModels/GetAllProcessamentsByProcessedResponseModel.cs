9ab8ed88e0103d325714226a44e6537d2fec0cc1
pedro-octavio <pedrooctavio.dev@outlook.com>
2021-03-04 12:17:27 -0300

    processament response models created

---
+++ src/CREDITProcessament.Domain/Models/ResponseModels/GetAllProcessamentsByProcessedResponseModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CREDITProcessament.Domain.Models.ResponseModels
+{
+    public class GetAllProcessamentsByProcessedResponseModel
+    {
+        public string UserCPF { get; set; }
+        public int Score { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
