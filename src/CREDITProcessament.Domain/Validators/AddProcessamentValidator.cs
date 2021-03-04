e07897af3f7a40878c6ddc4c7ec6d0adc8b11a48
pedro-octavio <pedrooctavio.dev@outlook.com>
2021-03-04 15:06:43 -0300

    validators added for processament request models

---
+++ src/CREDITProcessament.Domain/Validators/AddProcessamentValidator.cs
@@ -0,0 +1,16 @@
+﻿using CREDITProcessament.Domain.Models.RequestModels;
+using FluentValidation;
+
+namespace CREDITProcessament.Domain.Validators
+{
+    public class AddProcessamentValidator : AbstractValidator<AddProcessamentRequestModel>
+    {
+        public AddProcessamentValidator()
+        {
+            RuleFor(x => x.UserCPF)
+                .NotNull().WithMessage("The CPF cannot be null.")
+                .NotEmpty().WithMessage("The CPF cannot be empty.")
+                .Length(11).WithMessage("The of lenghtn of CPF is 11 caracters");
+        }
+    }
+}
---
+++ src/CREDITProcessament.Domain/Validators/DeleteRangeProcessamentValidator.cs
@@ -0,0 +1,16 @@
+﻿using CREDITProcessament.Domain.Models.RequestModels;
+using FluentValidation;
+
+namespace CREDITProcessament.Domain.Validators
+{
+    public class DeleteRangeProcessamentValidator : AbstractValidator<DeleteRangeProcessamentRequestModel>
+    {
+        public DeleteRangeProcessamentValidator()
+        {
+            RuleForEach(x => x.CPFs)
+                .NotNull().WithMessage("The CPF cannot be null.")
+                .NotEmpty().WithMessage("The CPF cannot be empty.")
+                .Length(11).WithMessage("The of lenghtn of CPF is 11 caracters");
+        }
+    }
+}
