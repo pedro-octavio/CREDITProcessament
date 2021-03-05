ee5cbda7bdf2efaa3be144c7a4bd190a238a8165
pedro-octavio <pedrooctavio.dev@outlook.com>
2021-03-05 14:39:13 -0300

    dashboard component created

--- src/Frontend/src/app/app.module.ts
+++ src/Frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 
+import { DashboardComponent } from './pages/dashboard/dashboard.component'
 import { NavbarComponent } from './pages/shared/navbar/navbar.component'
 
 @NgModule({
   declarations: [
     AppComponent,
+    DashboardComponent,
     NavbarComponent
   ],
   imports: [
