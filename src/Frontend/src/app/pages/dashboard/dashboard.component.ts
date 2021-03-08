726f9a197be956960e4045b1d870fdd86ea8b9d1
pedro-octavio <pedrooctavio.dev@outlook.com>
2021-03-08 20:27:51 -0300

    dashboard updated

--- src/Frontend/src/app/pages/dashboard/dashboard.component.ts
+++ src/Frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { GetAllUsersResponseModel } from 'src/app/models/responseModels/GetAllUsersResponseModel'
 
 @Component({
   selector: 'app-dashboard',
@@ -6,8 +7,29 @@ import { Component } from '@angular/core'
   styleUrls: ['./dashboard.component.css']
 })
 
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
   constructor() {
 
+  }
+
+  users: GetAllUsersResponseModel[]
+
+  ngOnInit(): void {
+    this.users = [
+      { name: 'Fernando Marcos Vinicius da Luz', cpf: '95592318049' },
+      { name: 'Sandra Eloá Vanessa da Silva', cpf: '25028704961' },
+      { name: 'Pedro Henrique Renato Antonio Ramos', cpf: '05018869585' },
+      { name: 'Antonio Gustavo Porto', cpf: '94784584727' },
+      { name: 'Calebe Juan Moreira', cpf: '98954405967' },
+      { name: 'Esther Tatiane Figueiredo', cpf: '73467889905' },
+      { name: 'Miguel Cláudio Lorenzo Castro', cpf: '21717101445' },
+      { name: 'Regina Isabela Larissa Moreira', cpf: '00621343706' },
+      { name: 'Manoel Theo Ribeiro', cpf: '00621343706' },
+      { name: 'César Lucas Enzo Jesus', cpf: '17098943185' },
+      { name: 'Giovanni Victor Cardoso', cpf: '09216712376' },
+      { name: 'Vanessa Elza Freitas', cpf: '93680444060' },
+      { name: 'Luís Luiz Nicolas Jesus', cpf: '92293953408' },
+      { name: 'Felipe Luís Cauê Araújo', cpf: '48833940420' },
+    ]
   }
 }
