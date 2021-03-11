58d7dfd154b59b034ea3d0817d3e238a447ba874
pedro-octavio <pedrooctavio.dev@outlook.com>
2021-03-11 16:44:57 -0300

    find user dialog stylied and logic implemented

---
+++ src/Frontend/src/app/models/requestModels/FindUserByCPFRequestModel.ts
@@ -0,0 +1,7 @@
+export class FindUserByCPFRequestModel {
+    constructor(cpf: string) {
+        this.cpf = cpf
+    }
+
+    cpf: string
+}
--- src/Frontend/src/app/pages/dialogs/find-user/find-user.component.ts
+++ src/Frontend/src/app/pages/dialogs/find-user/find-user.component.ts
@@ -1,13 +1,22 @@
 import { Component, OnInit } from '@angular/core'
-import { FormBuilder } from '@angular/forms'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 import { MatDialogRef } from '@angular/material/dialog'
 
 import { ToastrService } from 'ngx-toastr'
+import { FindUserByCPFRequestModel } from 'src/app/models/requestModels/FindUserByCPFRequestModel'
+import { UserValidator } from '../../validators/UserValidator'
 
 @Component({
   selector: 'app-find-user',
   templateUrl: './find-user.component.html',
-  styleUrls: ['./find-user.component.css']
+  styleUrls: ['./find-user.component.css',
+    '../../../../assets/css/elements.css',
+    '../../../../assets/css/margin.css',
+    '../../../../assets/css/buttons.css',
+    '../../../../assets/css/font-size.css',
+    '../../../../assets/css/width.css',
+    '../../../../assets/css/text-align.css',
+    '../../../../assets/css/shared.css']
 })
 
 export class FindUserComponent implements OnInit {
@@ -15,7 +24,34 @@ export class FindUserComponent implements OnInit {
 
   }
 
-  ngOnInit(): void {
+  userForm: FormGroup
 
+  user: FindUserByCPFRequestModel
+
+  ngOnInit(): void {
+    this.createFindUserForm()
+  }
+
+  createFindUserForm(): void {
+    this.userForm = this.formBuilder.group({
+      cpf: ['', Validators.compose([
+        Validators.required,
+        UserValidator.CPFValidation
+      ])]
+    })
+  }
+
+  find(): void {
+    const formData = this.userForm.value
+
+    const user = new FindUserByCPFRequestModel(formData.cpf)
+
+    this.toastrService.info('User Found Successfully.', 'User')
+
+    this.dialogRef.close()
+  }
+
+  get cpf(): any {
+    return this.userForm.get('cpf')
   }
 }
